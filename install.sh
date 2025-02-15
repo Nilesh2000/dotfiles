c812b28338edfe19f0cd22b0af379238df01a5a4
Nilesh D <42713643+Nilesh2000@users.noreply.github.com>
2025-02-15 20:30:19 +0530

    chore: add newline only inside if condition

--- install.sh
+++ install.sh
@@ -44,10 +44,10 @@ ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
     git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
     echo "✅ zsh-autosuggestions installed."
+    echo ""
 else
     echo "✅ zsh-autosuggestions already installed."
 fi
-echo ""
 
 if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
